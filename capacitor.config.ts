71f777a80c5dafba3c1abff5cf528613d30a4cd8
[dyad] <git@dyad.sh>
2026-07-08 18:57:47 +0800

    Add 1 file, update 3 files, remove 1 file

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ligtas.camnorte',
+  appName: 'Ligtas CamNorte',
+  webDir: 'dist'
+};
+
+export default config;
